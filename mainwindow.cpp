e37af11cc1822f54be19137a1a338057eff971e5
Danyal <mugutdinov.danyal@yandex.ru>
2015-11-22 23:41:04 +0300

    work paiint basic line

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -18,67 +18,59 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_pushButton_clicked()
-{
-    QVector<double> x(101), y(101); // initialize with entries 0..100
-       for (int i=0; i<101; ++i)
-       {
-         x[i] = i/50.0 - 1; // x goes from -1 to 1
-         y[i] = x[i]*x[i]; // let's plot a quadratic function
-       }
-       // create graph and assign data to it:
-       ui->widget->addGraph();
-       ui->widget->graph(0)->setData(x, y);
-       // give the axes some labels:
-       ui->widget->xAxis->setLabel("x");
-       ui->widget->yAxis->setLabel("y");
-       // set axes ranges, so we see all data:
-       ui->widget->xAxis->setRange(-1, 1);
-       ui->widget->yAxis->setRange(0, 1);
-       ui->widget->replot();
-}
 
 void MainWindow::f()
 {
 
-//the limit for triangular distribution
-        double below_limit = -10;
-        double top_limit = 10;
-        double max_position = /*(((top_limit - below_limit) / 2) + below_limit)*/ 6;
+    int dot_number = 150;
 
 //the limit for coordinate system
-        double left_x = -10;
-        double right_x = 10;
+     double left_x = 0;
+     double right_x = 100;
 
-        double top_y = -10;
-        double below_y = 10;
+     double top_y = 25;
+     double below_y = -10;
 
-// the number of points on the coordinate system
-        int dot_number = 10000;
-        double step = (right_x - left_x) / dot_number;
+     double h = 0.4;
 
-        QVector<double> x(dot_number), y(dot_number); // initialize with entries 0..100
-        for (int i=0; i<dot_number; ++i)
-        {
-          x[i] = i*step + left_x; // x goes from -1 to 1
-          /*cout<<*/(y[i] =  triangular(below_limit, top_limit, max_position)); // let's plot a quadratic function
-        }
-        // create graph and assign data to it:
-        ui->widget->addGraph();
-        ui->widget->graph(0)->setData(x, y);
 
-        ui->widget->graph(0)->setPen(QColor(50, 50, 50, 255));//задаем цвет точки
-        ui->widget->graph(0)->setLineStyle(QCPGraph::lsNone);//убираем линии
-        //формируем вид точек
-        ui->widget->graph(0)->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, 2));
+   QVector<double> x(dot_number), y(dot_number); // initialize with entries 0..100
 
-        // give the axes some labels:
-        ui->widget->xAxis->setLabel("x");
-        ui->widget->yAxis->setLabel("y");
-        // set axes ranges, so we see all data:
-        ui->widget->xAxis->setRange(left_x, right_x);
-        ui->widget->yAxis->setRange(below_y, top_y);
-        ui->widget->replot();
+   QVector<double> z1(dot_number+1 ),  z2(dot_number+1),  z3(dot_number+1);
+   double x_t = 2;/*double z1 = 0,  z2 = 0,  z3 = 0, z1_old, z2_old, z3_old, x_t = 2;*/
+   double k = 10, a1 = 2, a2 = 3, b1 = 4, b2 = 3;
+
+
+   for(int i = 1; i < dot_number; i++)
+   {
+
+       z1[i+1] = z1[i] +  h * z2[i];
+       z2[i+1] = z2[i] + h * z3[i];
+
+      // z3[i+1] =z3[i] + h * ((2 * b1 * b2 + b2*b2) * z3[i] + (b1 + 2 * b2) * z2[i] + z1[i] - x_t) / ((- b1) * b2 * b2);
+       z3[i+1] =z3[i] + h * ((x_t - (b2*b2 + 2 * b1 * b2) * z3[i] - (b1 + 2 * b2) * z2[i] - z1[i]) / (b1 * b2));
+
+        x[i] = i*h + left_x; // x goes from -1 to 1
+        //y[i] = k * (a1*a2 * z3[i] - (a1 - a2) * z2[i] + z1[i]);
+        y[i] = -(a1 * a2 * k) * z2[i+1] + z2[i]*(a1 - a2) + k*z1[i];
+
+   }
+
+
+  // create graph and assign data to it:
+    ui->widget->addGraph();
+    ui->widget->graph(0)->setData(x, y);
+
+    ui->widget->graph(0)->setPen(QColor(50, 50, 50, 255));//задаем цвет точки
+
+    // give the axes some labels:
+    ui->widget->xAxis->setLabel("x");
+    ui->widget->yAxis->setLabel("y");
+    // set axes ranges, so we see all data:
+    ui->widget->xAxis->setRange(left_x, right_x);
+    ui->widget->yAxis->setRange(below_y, top_y);
+    ui->widget->replot();
+
 }
 
 
